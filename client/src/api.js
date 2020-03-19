342abceca04107b3c1717467a223d0901e72da30
Joe Yan <chengmu.yan@gmail.com>
2020-03-19 18:17:41 -0400

    Add default argument page = 1 to api.getSearchResults

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = {
-  getSearchResults: async (searchTerm, page, limit = 20) => {
+  getSearchResults: async (searchTerm, page = 1, limit = 20) => {
     const config = {
       url: "/events",
       params: {
